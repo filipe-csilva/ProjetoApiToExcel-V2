c98b89e72f9fac23f3958f3e2cff15e628453071
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2023-10-01 14:47:54 -0300

    alteracao de testes dos campos vazios

--- ApiToExcel/FrmPessoFornecedores.cs
+++ ApiToExcel/FrmPessoFornecedores.cs
@@ -16,29 +16,62 @@ namespace ApiToExcel
             string urlEntrada = TxUrlAPI.Text;
             string url = $"https://{urlEntrada}";
 
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            try
             {
                 await UpdateTxtJson("Iniciando");
 
-                var credential = new PasswordCredential(
-                    Username: TxUser.Text,
-                    Password: TxPass.Text);
+                if (string.IsNullOrWhiteSpace(urlEntrada))
+                {
+                    await UpdateTxtJson("A url da API está vazia.\n\nTente novamente!");
+                    return;
+                }
 
-                await UpdateTxtJson("Solicitado o acesso.");
+                if (string.IsNullOrWhiteSpace(TxRouter.Text))
+                {
+                    await UpdateTxtJson("A Rota da API está vazia.\n\nTente novamente!");
+                    return;
+                }
 
-                using var varejoFacil = new VarejoFacilClient(
-                    baseAddress: url,
-                    passwordCredential: credential);
+                if (string.IsNullOrWhiteSpace(TxUser.Text))
+                {
+                    await UpdateTxtJson("O Usuario está vazio.\n\nTente novamente!");
+                    return;
+                }
 
-                await UpdateTxtJson("Acessando a o caminho da API.");
+                if (string.IsNullOrWhiteSpace(TxPass.Text))
+                {
+                    await UpdateTxtJson("A Senha está vazia.\n\nTente novamente!");
+                    return;
+                }
 
-                dynamic fornecedores = varejoFacil.GetFromRoute(TxRouter.Text);
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    var credential = new PasswordCredential(
+                        Username: TxUser.Text,
+                        Password: TxPass.Text);
 
-                await UpdateTxtJson("Criando Arquivo Excel.");
+                    await UpdateTxtJson("Solicitado o acesso.");
 
-                var io = new IOService();
-                io.WriteXlFile(fornecedores, saveFileDialog1.FileName);
-                await UpdateTxtJson("Arquivo Excel criado com sucesso.");
+                    using var varejoFacil = new VarejoFacilClient(
+                        baseAddress: url,
+                        passwordCredential: credential);
+
+                    await UpdateTxtJson("Acessando a o caminho da API.");
+
+                    dynamic fornecedores = varejoFacil.GetFromRoute(TxRouter.Text);
+
+                    await UpdateTxtJson("Criando Arquivo Excel.");
+
+                    var io = new IOService();
+                    io.WriteXlFile(fornecedores, saveFileDialog1.FileName);
+                    await UpdateTxtJson("Arquivo Excel criado com sucesso.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                UpdateTxtJson($"Erro na requisição: {ex.Message}");
+                throw ex;
             }
         }
 
